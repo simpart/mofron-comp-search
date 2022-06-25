c21933bdc8e4c69c5b7097e2f22ffd13ddffd1af
simpart <taria.ki4m@gmail.com>
2022-06-25 15:16:24 +0000

    add search event

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 const Input  = require("mofron-comp-input");
 const Button = require("mofron-comp-button");
+const comutl = mofron.util.common;
 
 module.exports = class extends mofron.class.Component {
     /**
@@ -21,6 +22,8 @@ module.exports = class extends mofron.class.Component {
             this.modname("Search");
 	    this.shortForm("text");
             
+            this.confmng().add("searchEvent", { type: "event", list: true });
+
 	    /* init config */
 	    if (0 < arguments.length) {
                 this.config(p1);
@@ -41,19 +44,28 @@ module.exports = class extends mofron.class.Component {
             super.initDomConts();
 	    this.style({ "display" : "flex" });
             
-            this.child([
-	        new Input({ type:"search", height: "0.31rem", accentColor:[180,180,180] }),
-		new Button({
-		    text:  "Search",
-		    style: { "position": "relative", "left": "-0.01rem" }
-		})
-            ]);
+	    this.button().config({ text:"Search" });
+            this.child([this.input(), this.button()]);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    input (prm) {
+        try {
+	    if (true === comutl.isinc(prm, "Input")) {
+                prm.config({
+                    type: "search", height: "0.31rem", accentColor: [180,180,180]
+		});
+	    }
+            return this.innerComp("input", prm, Input);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
     text (ph, btn) {
         try {
             this.placeholder(ph);
@@ -76,6 +88,30 @@ module.exports = class extends mofron.class.Component {
 	}
     }
     
+    button (prm) {
+        try {
+            if ("string" === typeof prm) {
+                this.button().text(prm);
+                return;
+            } else if (true === comutl.isinc(prm, "Button")) {
+                prm.style({ "position": "relative", "left": "-0.01rem" });
+		prm.clickEvent((p1,p2,p3) => {
+		    let evt = p3.searchEvent();
+		    for (let eidx in evt) {
+                        evt[eidx][0](
+			    p3,
+			    p3.input().value(),
+			    evt[eidx][1]);
+		    }
+		}, this);
+	    }
+            return this.innerComp("button", prm, Button);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+
     buttonText (prm) {
         try {
             let btn = this.child()[1];
@@ -102,6 +138,28 @@ module.exports = class extends mofron.class.Component {
             console.error(e.stack);
             throw e;
         }
+    }
+
+    height (prm) {
+        try {
+            this.child()[0].height(prm);
+	    return this.child()[1].height(comutl.sizediff(prm,"0.06rem"));
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    searchEvent (fnc,prm) {
+        try {
+            return this.confmng(
+	        "searchEvent",
+		(undefined === fnc) ? undefined : [fnc,prm]
+            );
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
     }
 }
 /* end of file */
